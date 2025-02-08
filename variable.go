2a425b2a8e99505792196b3a141babfdcc9b5917
useryesa9@gmail.com <useryesa9@gmail.com>
2025-02-08 15:52:47 +0700

    var

--- variable.go
+++ variable.go
@@ -8,10 +8,24 @@ func main() {
 	nama := "muhamad yesa"
 	umur := 17
 	uang := 0.2
-	
+
 	nama = "yesnot"
 
 	fmt.Println(nama)
 	fmt.Println(umur)
 	fmt.Println(uang)
+
+	var a, b, c int = 1, 1, 1
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	var (
+		num int = 17
+		str string = "nullablenone"
+	)
+
+	println(num)
+	println(str)
 }
