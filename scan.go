c058f5d3f42ccc7b7f27565c542347a2beb11caa
useryesa9@gmail.com <useryesa9@gmail.com>
2025-02-08 08:54:03 +0700

    scan

---
+++ scan.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nilai int
+
+	// fmt.Scanln(&nilai)
+	// fmt.Println("umur mu : ", nilai)
+
+	fmt.Scanf("%d", &nilai)
+	fmt.Println("umur mu : ", nilai)
+}
