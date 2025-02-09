011f72e780a2aa48330884808f21251bd03965f1
useryesa9@gmail.com <useryesa9@gmail.com>
2025-02-09 15:55:59 +0700

    slice update

--- slice.go
+++ slice.go
@@ -3,19 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	//array
-	var angka = [5]int{1,2,3,4,5}
-	slice := angka[1:5]
-	fmt.Println(slice)
+	angka := []int{1,2,3,4,5}
 
-	// langsung
-	slice_langsung := []int{1,2,3,4,5}
-	fmt.Println(slice_langsung)
-
-	// pake mathod
-	slice_mathod := make([]int, 3, 5)
-													// len, cap
-	fmt.Println(slice_mathod)
-	fmt.Println(len(slice_mathod))
-	fmt.Println(cap(slice_mathod))
+	fmt.Println(len(angka))
+	fmt.Println(cap(angka))
 }
